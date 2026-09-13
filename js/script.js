fa10c82f8372d8091d2a85746bacc69d6eaeb977
cloudlab1128 <cloud.lab6677@gmail.com>
2026-09-14 01:18:15 +0530

    Restore website script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-/* Social icons are restored by commit c557809. */
+const nav = document.querySelector('#mainNav');
+const toggle = document.querySelector('.menu-toggle');
+toggle?.addEventListener('click', () => { const open = nav.classList.toggle('open'); toggle.setAttribute('aria-expanded', open); });
+document.querySelectorAll('.nav a').forEach(a => a.addEventListener('click', () => nav.classList.remove('open')));
